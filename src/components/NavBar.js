7a12540f7fe012537946af27b59f50ae07649c92
darrenjcarrillo <darrencarrillo@Aarons-MBP.lan>
2021-09-17 10:54:46 -0700

    navbar button styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,26 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import './NavBar.css';
 
 function NavBar() {
   // Menu bar state
   const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
   // Toggle menue bar
   const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+
+  }
+
+  window.addEventListener('resize', showButton)
 
 
   return (
@@ -20,9 +35,19 @@ function NavBar() {
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item">
-              <Link to="/" className='nav-links' onClick={closeMobileMenu}> </Link>
+              <Link to="/" className='nav-links' onClick={closeMobileMenu}>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className='nav-links' onClick={closeMobileMenu}>Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className='nav-links' onClick={closeMobileMenu}>Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/sign-up" className='nav-links-mobile' onClick={closeMobileMenu}>Sign Up</Link>
             </li>
           </ul>
+          {button && <Button buttonSyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
