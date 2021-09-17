662bef480305e72d8898f406327b1309bb838db2
darrenjcarrillo <darrencarrillo@Aarons-MBP.lan>
2021-09-17 10:16:41 -0700

    Menu bar toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,12 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function NavBar() {
+  // Menu bar state
+  const [click, setClick] = useState(false)
+  // Toggle menue bar
+  const handleClick = () => setClick(!click)
+
+
   return (
     <>
       <nav className="navbar">
@@ -9,9 +15,14 @@ function NavBar() {
           <Link to="/" className="navbar-logo">
             TRVL <i className="fab fa-typo3"/>
           </Link>
-          <div className="menu-icon">
+          <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className='nav-links' onClick={closeMobileMenu}> </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
